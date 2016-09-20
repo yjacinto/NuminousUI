f805ea15294b95208212405e3ffda7218519b5c6
Elaine Yu <laineeyu@gmail.com>
2016-09-19 21:20:21 -0700

    Sign up form in about section, schedule tab not doing anything when clicked

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,9 +32,32 @@ angular.module('numinousUiApp', [
       templateUrl:'/views/dashboard.html'
 
     }
+    
+    var aboutState = {
+      name: 'about',
+      url: '/about',
+      templateUrl:'/views/about.html'
+
+    }
+    
+    var scheduleState = {
+      name: 'schedule',
+      url: '/schedule',
+      templateUrl:'/views/schedule.hmtl'
+    }
+    
+        var extraState = {
+      name: 'extra',
+      url: '/extra',
+      templateUrl:'/views/extra.html'
+
+    }
 
     $stateProvider.state(dashboardState);
     $stateProvider.state(loginState);
+    $stateProvider.state(aboutState);
+    $stateProvider.state(scheduleState);
+    $stateProvider.state(extraState);
 
 
   });
